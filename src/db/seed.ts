246056e3aa0a6129acfda95a6f2730adced3a76a
pedrovicto <victorpedrosilva445@gmail.com>
2025-07-09 12:37:39 -0300

    feat: add rooms listing

---
+++ src/db/seed.ts
@@ -0,0 +1,23 @@
+// biome-ignore assist/source/organizeImports: <expla>
+import { reset, seed } from 'drizzle-seed'
+import { schema } from './schema/index.ts'
+import { db, sql } from './connection.ts'
+
+await reset(db,  schema )
+
+await seed(db,  schema ).refine(f => {
+    return {
+        rooms: {
+            count: 20,
+            columns: {
+                name: f.companyName(),
+                description: f.loremIpsum(),
+            },
+        },
+    }
+})
+
+await sql.end()
+
+// biome-ignore lint/suspicious/noConsole: only used in dev
+console.log('database seeded')
